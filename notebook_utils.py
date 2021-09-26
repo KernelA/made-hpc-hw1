70ebe4016601ca089a7c00184a6c7a964f8d041b
KernelA <17554646+KernelA@users.noreply.github.com>
2021-09-26 23:09:57 +0300

    Solution of part 2 added

--- notebook_utils.py
+++ notebook_utils.py
@@ -1,3 +1,4 @@
+from typing import Union, Optional
 import numpy as np
 
 
@@ -7,3 +8,20 @@ def log_transform(data):
 
 def inverse_log_transform(data):
     return 10 ** data
+
+
+def twatt2kwatt(value):
+    return value * 1e9
+
+
+def comma_number2str(number: str) -> Optional[Union[int, float]]:
+    stripped_value = number.replace(",", "")
+    try:
+        value = int(stripped_value)
+    except ValueError:
+        try:
+            value = float(stripped_value)
+        except ValueError:
+            return None
+
+    return value
